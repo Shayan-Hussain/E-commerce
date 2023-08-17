23ab4f440c6d47712f760efa09e10cf176b356e2
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-08-17 14:43:12 -0400

    Made the navbar responsive.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-if (window.innerWidth <= 700) {
-    document.querySelector(".menu_btn").ariaHidden = "";
+
+let nav_hidden = false;
+
+const menu_btn_cta = () => {
+
+    if (nav_hidden == false) {
+        let nav_bar = document.querySelector(".nav_bar");
+
+        nav_bar.style.display = "none";
+
+        let header = document.querySelector("header");
+
+        header.style.height = "60px";
+
+        nav_hidden = true;
+    }   else    {
+        let nav_bar = document.querySelector(".nav_bar");
+
+        nav_bar.style.display = "flex";
+
+        let header = document.querySelector("header");
+
+        header.style.height = "360px";
+
+        nav_hidden = false;
+    }
+
 }
