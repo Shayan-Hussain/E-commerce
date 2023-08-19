c6f7c8a31557f58336ad631dce0c6be94953d0db
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-08-18 23:38:35 -0400

    added footer

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 
-let nav_hidden = false;
+let nav_bar = document.querySelector(".nav_bar");
+let header = document.querySelector("header");
+
+
+let nav_hidden = true;
+
+if (window.innerWidth <= "700") {
+    nav_bar.style.display = "none";
+    header.style.height = "60px";
+}   else    {
+    nav_bar.style.display = "flex";
+}
 
 const menu_btn_cta = () => {
 
     if (nav_hidden == false) {
-        let nav_bar = document.querySelector(".nav_bar");
-
         nav_bar.style.display = "none";
-
-        let header = document.querySelector("header");
 
         header.style.height = "60px";
 
         nav_hidden = true;
     }   else    {
-        let nav_bar = document.querySelector(".nav_bar");
-
         nav_bar.style.display = "flex";
-
-        let header = document.querySelector("header");
 
         header.style.height = "360px";
 
